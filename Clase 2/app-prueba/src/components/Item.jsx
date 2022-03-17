369bbd10a92851ac094a252c505cbf6ccf42b5f6
Theasasin7 <ulises.cruz7@hotmail.com>
2022-03-16 23:30:55 -0400

    reparando stock de itemcount en archivo de item

--- Clase 2/app-prueba/src/components/Item.jsx
+++ Clase 2/app-prueba/src/components/Item.jsx
@@ -4,7 +4,7 @@ import { Button } from '../helper/Button'
 import ItemCount from './ItemCount'
 
 
-export const Item = ({id, name, category, img, price}) => {
+export const Item = ({id, name, category, img, price, stock}) => {
     return (
         <div className="flex flex-row justify-center">
             <div className="flex flex-col p-2 gap-2 bg-gray-500 m-5 border-2 border-gray-700">
@@ -14,7 +14,7 @@ export const Item = ({id, name, category, img, price}) => {
                     <p className="flex justify-center text-white">{category}</p>
                     <p className="flex justify-center text-white text-sm">USD ${price}</p>
                     <Link to={`/details/${id}`}><Button>Game Details</Button></Link>
-                    <ItemCount stock="50" initial="1" />
+                    <ItemCount stock={stock} initial="1" />
                 </div>
             </div>
         </div>
