aeb7ff9e89ce2e8c341d5251d3ff7357a70d63f4
Theasasin7 <ulises.cruz7@hotmail.com>
2022-04-07 13:30:36 -0400

    finalizando proyecto final

--- Clase 2/app-prueba/src/helper/AccessData.jsx
+++
@@ -1,8 +0,0 @@
-import { stock } from "../components/Data"
-export const askData = () =>{
-    return new Promise( (resolve)=>{
-        setTimeout(()=>{
-            resolve(stock)
-        },2000)
-    })
-}
